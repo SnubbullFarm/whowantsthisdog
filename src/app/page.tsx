8719f26a9c744f2b15f1c3a88672dfbd05e8d6e1
SnubbullTrouble <snubbulltrouble@gmail.com>
2024-03-10 01:37:19 -0500

    first build/run of the new configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,87 +1,56 @@
-import { unstable_noStore as noStore } from "next/cache";
-import Link from "next/link";
+"use client";
 
-import { CreatePost } from "whowantsthisdog/app/_components/create-post";
-import { getServerAuthSession } from "whowantsthisdog/server/auth";
-import { api } from "whowantsthisdog/trpc/server";
-import styles from "./index.module.css";
+import FullWidthGrid from "./_components/Results";
+import Cat from "./_components/Breeds";
+import Listing from "./_components/Listing";
+import Sidebar from "./_components/SideBar";
+import { Box, Stack } from "@mui/material";
+import NavBar from "./_components/NavBar";
+import User from "./_components/User";
+import { useState } from "react";
+import { State } from "./_components/State";
+import About from "./_components/About";
+import { ThemeProvider, useTheme } from "@emotion/react";
+import { theme } from "./_components/theme";
 
-export default async function Home() {
-  noStore();
-  const hello = await api.post.hello.query({ text: "from tRPC" });
-  const session = await getServerAuthSession();
-
+function App() {
+  const [state, setState] = useState<State>(State.About);
+  const listing = new Listing(
+    0,
+    "cat",
+    "sweet cat needs new home",
+    "Macon",
+    "https://th-thumbnailer.cdn-si-edu.com/bgmkh2ypz03IkiRR50I-UMaqUQc=/1000x750/filters:no_upscale():focal(1061x707:1062x708)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/55/95/55958815-3a8a-4032-ac7a-ff8c8ec8898a/gettyimages-1067956982.jpg",
+    Cat.Maine_Coon.toString(),
+    "@snubbulltrouble"
+  );
+  const props: Listing[] = [listing];
+  const user = new User(
+    "@snubbulltrouble",
+    "Macon",
+    ["God", "Emporer", "King"],
+    undefined
+  );
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>
-          Create <span className={styles.pinkSpan}>T3</span> App
-        </h1>
-        <div className={styles.cardRow}>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>First Steps →</h3>
-            <div className={styles.cardText}>
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>Documentation →</h3>
-            <div className={styles.cardText}>
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className={styles.showcaseContainer}>
-          <p className={styles.showcaseText}>
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-
-          <div className={styles.authContainer}>
-            <p className={styles.showcaseText}>
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className={styles.loginButton}
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
-          </div>
-        </div>
-
-        <CrudShowcase />
-      </div>
-    </main>
+    <>
+      <ThemeProvider theme={theme}>
+        {state === State.Results ? (
+          <Box bgcolor={"background.default"} color={"text.primary"}>
+            <NavBar user={user}></NavBar>
+            <Stack direction="row" spacing={2} justifyContent="space-between">
+              <Sidebar state={state} setState={setState}></Sidebar>
+              <FullWidthGrid items={props}></FullWidthGrid>
+            </Stack>
+          </Box>
+        ) : null}
+        {state === State.About ? (
+          <Stack direction="column">
+            <About state={state} setState={setState}></About>
+          </Stack>
+        ) : null}
+      </ThemeProvider>
+    </>
   );
 }
 
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
-
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className={styles.showcaseContainer}>
-      {latestPost ? (
-        <p className={styles.showcaseText}>
-          Your most recent post: {latestPost.name}
-        </p>
-      ) : (
-        <p className={styles.showcaseText}>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
-  );
-}
+export default App;
